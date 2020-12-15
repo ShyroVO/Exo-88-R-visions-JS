e6bbc52b9f62e3e121f9ea6ad9bae796c4745e37
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-15 22:02:33 +0100

    1.0

--- exercice1.js
+++ exercice1.js
@@ -1,4 +1,5 @@
 let tableau = [];
+let afficheTab = document.getElementById('divAfficheTableau');
 
 document.getElementById('valeurRandom').innerHTML = "entrez au moins 10 valeurs";
 
@@ -10,13 +11,14 @@ document.getElementById('valider').addEventListener('click', function (){
     let textUtilisateur = document.getElementById('textUtilisateur').value;
     tableau.push(textUtilisateur);
 
+    console.log(tableau.length);
+
     if (tableau.length === 10) {
         valeurHasard = getRandom();
 
         document.getElementById('valeurRandom').innerHTML = "Valeur au hasard: " + tableau[valeurHasard];
         document.getElementById('valeur5').innerHTML = "5eme valeur: " + tableau[4];
     }
-
 })
 
 document.getElementById('boutonAfficherHasard').addEventListener('click', function (){
@@ -26,9 +28,34 @@ document.getElementById('boutonAfficherHasard').addEventListener('click', functi
 })
 
 document.getElementById('afficherTableau').addEventListener('click', function (){
-    let tableauJoin = tableau.join(' - ');
-    let numerosIndex = 0;
 
-    document.getElementById('divAfficheTableau').innerHTML = numerosIndex + tableauJoin;
+    afficheTab.innerHTML = "";
+
+    for (let i = 0 ; i < tableau.length ; i++) {
+        afficheTab.innerHTML += i + " - " + tableau[i] + "<br>";
+    }
+
+})
+
+// Reset affichage Tableau:
+function affichageTableau (){
+    afficheTab.innerHTML = "";
+
+    for (let i = 0 ; i < tableau.length ; i++) {
+        afficheTab.innerHTML += i + " - " + tableau[i] + "<br>";
+    }
+}
+
+// Sup dernier:
+document.getElementById('supLast').addEventListener('click', function (){
+    tableau.splice(-1 , tableau.length);
+    affichageTableau();
+
+})
+
+// Sup All:
+document.getElementById('supAll').addEventListener('click', function (){
+    tableau.splice(0, tableau.length);
+    affichageTableau();
 
 })
