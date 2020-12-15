b25621cda35f19e5f96e3e9e1f22d4c0ba2c9f84
ShyroVO <bosquet.laurie@outlook.com>
2020-12-15 16:40:32 +0100

    0.7

---
+++ exercice1.js
@@ -0,0 +1,33 @@
+let tableau = [];
+
+document.getElementById('valeurRandom').innerHTML = "entrez au moins 10 valeurs";
+
+function getRandom(){
+    return Math.floor(Math.random() * tableau.length);
+}
+
+document.getElementById('valider').addEventListener('click', function (){
+    let textUtilisateur = document.getElementById('textUtilisateur').value;
+    tableau.push(textUtilisateur);
+
+    if (tableau.length === 10) {
+        valeurHasard = getRandom();
+
+        document.getElementById('valeurRandom').innerHTML = "Valeur au hasard: " + tableau[valeurHasard];
+        document.getElementById('valeur5').innerHTML = "5eme valeur: " + tableau[4];
+    }
+
+})
+
+document.getElementById('boutonAfficherHasard').addEventListener('click', function (){
+    valeurHasard2 = getRandom();
+
+    document.getElementById('afficherHasard').innerHTML = tableau[valeurHasard2];
+})
+
+document.getElementById('afficherTableau').addEventListener('click', function (){
+    
+
+    document.getElementById('divAfficheTableau').innerHTML = tableau;
+
+})
